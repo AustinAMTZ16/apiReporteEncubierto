12e12f9a9351d0b208770065d1a799388dad3a67
AustinAMTZ16 <austintv52@gmail.com>
2023-04-25 11:56:49 -0600

    Update Connection.php
    Datos para cadena de conexión SSH

--- connection/Connection.php
+++ connection/Connection.php
@@ -12,4 +12,28 @@
                     die('Error al conectarse a la BD');
         }//end __construct
     }//end class Connection
+
+
+    /*
+    
+    
+        $ssh = ssh2_connect('66.172.11.28');
+        ssh2_auth_password($ssh, 'synergy', 'Tis03#181');
+        $stream = ssh2_tunnel($ssh, '66.172.11.28', 3306);
+        $link = new MysqlStreamDriver($stream, 'synergy', 'Syn03#181', 'administrabajo');
+        $link->query('SELECT * FROM ...')->fetch_assoc();
+    
+        ip del server: 66.172.11.28
+        
+        ssh:
+        u= synergy
+        p= Tis03#181
+        Puerto-shh: 22
+        
+        Base de datos:
+        u= synergy
+        p= Syn03#181
+        bd: administrabajo
+        Puerto-bd: 3307
+    */
 ?>
