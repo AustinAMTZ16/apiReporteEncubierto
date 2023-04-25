b5ccfc50794514d286e8eb053f06553e94155140
AustinAMTZ16 <austintv52@gmail.com>
2023-04-24 20:42:13 -0600

    Prueba de ID + TRUE

--- models/ReporteEncubierto.php
+++ models/ReporteEncubierto.php
@@ -225,7 +225,7 @@
             $db->query($query);
             if($db->affected_rows) {
                 $last_id = $db->affected_rows;
-                return true + $last_id;
+                return $last_id;
             }//end if
             return FALSE;
         }//end insertReporteEncubierto
